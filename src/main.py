a1249366307b033625dc35f4894f17f7b42fc195
paulb730 <50187670+paulb730@users.noreply.github.com>
2025-03-11 18:01:47 -0500

    Ins

---
+++ src/main.py
@@ -0,0 +1,15 @@
+"""_summary_
+     La tienda de venta de artículos de regalo “gifty”, lleva su control de inventario a través de hojas
+de Excel, registrando las entradas de artículos y salidas de cada uno cada vez que se vende un
+artículo. Ustedes como equipo de desarrolladores de software en Python deben desarrollar un
+sistema sencillo de gestión de inventario. El sistema permitirá a los usuarios administrar el
+inventario de la tienda, utilizando los conceptos aprendidos en las cuatro unidades del
+diplomado en Python, incluyendo el uso de variables, estructuras de control de flujo, funciones
+y módulos, y estructuras de datos.
+
+"""
+"""Funciones administrar el inventario de la tienda"""
+
+
+
+
