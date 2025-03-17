21daa685408746c2873bb5def13db811516c8c75
Paul Benavides <pbenavides@KRUGER.COM.EC>
2025-03-17 18:12:51 -0500

    Funciones pseudocodigo

--- src/main.py
+++ src/main.py
@@ -14,7 +14,35 @@ y módulos, y estructuras de datos.
 print("hola mundo")
 
 """
-tres primeras funciones Paul
+1) Agregar productos: 
+    definir agregar_producto
+         Input de nombre, precio, descripcion, cantidad 
+         guardar en una estructura de datos (diccionario)
+         el ID sera generado automáticamente (puede ser un número secuencial)
+         print (desea agregar otro producto?)
+            si respuesta es si, repetir proceso de agregar producto
+            si respuesta es no, salir del proceso de agregar producto
+
+2) Actualizar productos:
+    Definir actualizar_producto
+        Input de ID del producto
+        Si el producto existe
+            Input de nombre, precio, descripcion, cantidad
+            Actualizar la información del producto
+            print (producto actualizado)
+            
+        En caso negativo, mostrar “Producto no encontrado”
+
+3) Eliminar productos:
+    Definir eliminar_producto
+        Input de ID del producto
+        Si el producto existe
+            Eliminar el producto
+            print (producto eliminado)
+            print (desea eliminar otro producto?)   
+                si respuesta es si, repetir proceso de eliminar producto
+                si respuesta es no, salir del proceso de eliminar producto            
+        En caso negativo, mostrar “Producto no encontrado”
 
 """
 
